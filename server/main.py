958fdb5007f90b5d4bf7e53d1a91cb87dc84086b
gaofan <gaofan@mumulife.com>
2017-06-23 15:06:48 +0800

    添加多clinet及数据库支持

--- server/main.py
+++ server/main.py
@@ -1,58 +1,102 @@
-from flask import request, Flask, Response
+from flask import request, Request, Flask, Response
 import config
 from functools import wraps
+from enum import Enum
+import pymongo
+import json
 
-
-def check_auth(username, password):
-    if config.NEED_AUTH:
-        return username == config.AUTH_USER and password == config.AUTH_PASSWORD
-    else:
-        return True
-
-
-def authenticate():
-    return Response(
-        'Could not verify your access level for that URL.\n'
-        'You have to login with proper credentials', 401,
-        {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-
-    return decorated
-
+class ClientType(Enum):
+    Client = 'proxy'
+    Spider = 'spider'
 
 app = Flask(__name__)
 
 
-@app.route('/record/<key>', methods=['GET'])
-def record(key):
-    if key == config.KEY:
-        ip = request.remote_addr
-        with open('ip', 'w') as f:
-            f.write(ip)
-            f.close()
-        return ip + '\n'
+def check_auth(token, client:ClientType):
+        if client == ClientType.Client:
+            return token == config.CLIENT_AUTH_TOKEN
+        elif client == ClientType.Spider:
+            return token == config.SPIDER_AUTH_TOKEN
+        else:
+            return False
+
+def authenticate(request:Request):
+    auth_token = request.headers.get('Authorization')
+    client = request.args.get('client')
+    if client and auth_token:
+        try:
+            client = ClientType(client)
+            res = check_auth(auth_token, client)
+            if res:
+                return True
+        except:
+            print('wrong keytype')
+            return False
+
+
+def get_collection()->pymongo:
+
+    client = pymongo.MongoClient('127.0.0.1', 27017)
+    db = client.proxy
+    collection = db.proxy_clients
+
+    return collection
+
+def jsonres(data = None, message= '', status= 200):
+    if data:
+        return Response(json.dumps({
+            'status':status,
+            'data': data
+        }),content_type='application/json')
     else:
-        return 'Invalid Key'
+        return Response(json.dumps({
+            'status': status,
+            'message': message
+        }),content_type='application/json')
+
+@app.route('/proxy', methods=['POST'])
+def update():
+    ip = request.remote_addr
+    port = request.form.get('port')
+    client_id = request.form.get('client_id')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if not client_id or not username or not password or not port:
+        return jsonres(message='参数不全', status=400)
+
+    if not authenticate(request):
+        return jsonres(message='认证失败', status=400)
+
+    collection = get_collection()
+
+    # 存入数据库
+    if collection.find_one({'client_id':client_id}):
+        collection.update({"client_id":client_id},{"$set":{"ip":ip, "port":port, "username":username, "password":password}})
+    else:
+        collection.insert({"client_id":client_id, "ip":ip, "port":port, "username":username, "password":password})
+
+    return jsonres(data={'client_id':client_id}, status=200)
 
 
-@app.route('/', methods=['GET'])
-@requires_auth
+@app.route('/proxy', methods=['GET'])
 def proxy():
-    with open('ip', 'r') as f:
-        ip = f.read().strip()
-        f.close()
-        if ip:
-            return ip + ':' + str(config.PORT)
-    return '0'
+    if not authenticate(request):
+        return jsonres(message='认证失败', status=400)
 
+    collection = get_collection()
+
+    cur = collection.find({},{'_id':False})
+    if cur:
+        proxylist = []
+        for proxy in cur:
+            proxylist.append(proxy)
+
+        if not proxylist:
+            return jsonres(status=200, data=[])
+        else:
+            return jsonres(status=200, data=proxylist)
+    else:
+        return jsonres(status=200, data=[])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=config.PORT)
